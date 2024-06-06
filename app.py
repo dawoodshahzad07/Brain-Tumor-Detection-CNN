7ff461074dc86c2735200bed7481853ef72c5f0c
Dawid <57856214+dawoodshahzad07@users.noreply.github.com>
2024-06-06 19:29:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
-
-
 import streamlit as st
 from PIL import Image 
 from prediction import mri_classification
 import os
+
+# Title and header
 st.title("Brain Tumor or Healthy Brain")
 st.header("Brain Tumor MRI Classifier")
 st.text("Upload a brain MRI Image for image classification as tumor or Healthy Brain")
-# Add a color scheme
-st.markdown('<style>body {background-color: #f0f0f0;}</style>', unsafe_allow_html=True)
 
-
+# File uploader
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg","png","jpeg"])
-st.markdown("""    ###### Credits:
-    - Osama Raheem
-    - Dawood Shahzad
-    - KongKhan""")
+
+# Credits
+st.markdown("""###### Credits:
+- Osama Raheem
+- Dawood Shahzad
+- KongKhan
+""")
+
+# Default background color
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #f0f0f0;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# If a file is uploaded
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
@@ -27,11 +42,28 @@ if uploaded_file is not None:
     
     st.write("Result:")
     
+    # Dynamically change background color based on the result
     if label == 0:
+        st.markdown(
+            """
+            <style>
+            body {
+                background-color: #d4edda;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
         st.write('<p style="color:green;">The MRI scan shows a healthy brain</p>', unsafe_allow_html=True)
     else:
+        st.markdown(
+            """
+            <style>
+            body {
+                background-color: #f8d7da;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
         st.write('<p style="color:red;">The MRI scan detects a brain tumor</p>', unsafe_allow_html=True)
-
-
-        
-        
